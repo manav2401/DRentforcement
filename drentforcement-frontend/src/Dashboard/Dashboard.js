1fb257edaace613a95f41244c18a28e771a6fe57
manav2401 <manavdarji.india@gmail.com>
2020-08-24 17:49:26 +0530

    auth check in pages

--- drentforcement-frontend/src/Dashboard/Dashboard.js
+++ drentforcement-frontend/src/Dashboard/Dashboard.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 import Web3 from 'web3';
 import Button from '@material-ui/core/Button';
 import detectEthereumProvider from '@metamask/detect-provider';
-// import { BrowserRouter as Router, Route, Switch, Link, Redirect } from 'react-router-dom';
+import { Redirect } from 'react-router-dom';
 
 // local imports
 // import { address, abi } from '../contractArtifacts';
@@ -13,88 +13,85 @@ import './Dashboard.css';
 var web3 = undefined;
 var userAccount = undefined;
 
-class Dashboard extends Component {
+class Dashboard extends React.Component {
 
     state = {
-        loading: false
+        isAuth: false,
+        isValid: false,
+        isMetamaskInstalled: false,
     };
 
-    handleClick = async () => {
+    async componentDidMount() {
         const provider = await detectEthereumProvider();
 
         if (provider) {
-            await this.fetchWalletDetails(provider);
+
+            this.setState({ isMetamaskInstalled: true });
+            if (provider !== window.ethereum) {
+                this.setState({ isValid: false })
+                alert('Multiple wallets are installed!');
+                return;
+    
+            } else {
+                console.log('Single account!');
+                this.state({ isValid: true });
+                var connected = provider.isConnected();
+                if (connected) {
+                    this.state({ isAuth: true });
+                    // fetch accounts and perform actions
+                } else {
+                    this.state({ isAuth: false });
+                    // redirect to another page
+                }
+            }
+        
         } else {
+            this.state({ isMetamaskInstalled: false });
             window.alert('Please install MetaMask!');
             return;
         }
-    }
 
-    async fetchWalletDetails(provider) {
 
-        if (provider !== window.ethereum) {
-            alert('Multiple wallets are installed!');
-        } else {
-            console.log('Single account!');
-            try {
-
-                // testing for isConnected!
-                var connected = provider.isConnected();
-                console.log("is connected: " + connected);
-
-                /*
-                // allowing user to connect and fetch accounts
-                const accounts = await provider.request({ method: 'eth_requestAccounts' });
-                userAccount = accounts[0];
-
-                // personal message sign
-                web3 = new Web3(provider);  // create a new web3 instance
-
-                var message = "Hello friend, Please sign this message!";
-
-                var result = await web3.eth.personal.sign(
-                    message,
-                    userAccount,
-                    function (err, result) {
-                        if (err) return console.log('error: ' + err);
-                    }
-                );
-
-                console.log('Result received: ' + result);
-
-                // set the state
-                this.setState({ loading: true });
-                */
-
-            } catch (error) {
-                // the case if user denies account access
-                console.log('Error: ' + error)
-                alert('Please create / select a wallet account to proceed further!');
-            }
-        }
-    }
-
-    tempFunction() {
-        console.log('Temp function called!');
     }
 
     render() {
-        this.tempFunction();
-        const { loading } = this.state;
-        return (
-            <div className="Dashboard">
-                <div className="message">
-                    <p>For performing this operation, you need to Connect your Metamask Wallet!</p>
-                </div>
+        this.componentDidMount();
+        const { isValid } = this.state;
+        const { isAuth } = this.state;
+        const { isMetamaskInstalled } = this.state;
 
-                <div className="metamask-login-button">
-                    <Button variant="contained" color="primary" onClick={this.handleClick} disableElevation>
-                        {loading ? `Connected ${userAccount}` : 'Connect with metamask'}
-                    </Button>
+        if (isMetamaskInstalled) {
+            if (isValid) {
+                if (!isAuth) {
+                    return (
+                        <div className="Dashboard">
+                            {/* <Redirect to="/auth" /> */}
+                            <h3>Redirect</h3>
+                        </div>
+                    )
+                } else {
+                    return (
+                        <div className="Dashboard">
+                            <h3>Welcome to D-rentforcement</h3>
+                        </div>
+                    )
+                }
+            } else {
+                return (
+                    <div className="Dashboard">
+                        <h3>Multiple wallets are installed! This dapp is only compatible with Metamask!</h3>
+                    </div>
+                )
+            }
+        } else {
+            return (
+                <div className="Dashboard">
+                    <h3>Please install a compatible wallet such as Metamask!</h3>
                 </div>
-            </div>
+            )
 
-        )
+        }
+
     }
 
 }
